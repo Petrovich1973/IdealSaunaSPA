1df5119e3ff2df03990578f5c05c74cd89886ecf
Petrovich1973 <designtorg@ya.ru>
2017-09-16 14:03:38 +0300

    build config edit plugins

--- webpack.config.js
+++ webpack.config.js
@@ -50,16 +50,16 @@ module.exports = {
             }
         ]
     },
-    // plugins: [
-    //     new webpack.DefinePlugin({
-    //         'process.env': {
-    //             'NODE_ENV': JSON.stringify('production')
-    //         }
-    //     }),
-    //     new webpack.optimize.UglifyJsPlugin({
-    //         compress: {
-    //             warnings: true
-    //         }
-    //     })
-    // ]
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: true
+            }
+        })
+    ]
 };
